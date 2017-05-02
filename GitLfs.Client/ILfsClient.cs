ea17521351e57dafd3bf32dbb3beab2e72b59228
Glenn Watson <glenn@glennwatson.net>
2017-05-02 19:56:16 +1000

    Initial commit

---
+++ GitLfs.Client/ILfsClient.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ILfsClient.cs" company="Glenn Watson">
+//   Copyright (C) 2017. Glenn Watson
+// </copyright>
+// <summary>
+//   Represents a GIT LFS client.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GitLfs.Client
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a GIT LFS client. 
+    /// </summary>
+    public interface ILfsClient
+    {
+        /// <summary>
+        /// Downloads a file.
+        /// </summary>
+        /// <param name="objectId">The object id of the object.</param>
+        /// <returns>The location on the local file system where the file is located.</returns>
+        Task<string> DownloadFile(string objectId);
+
+        /// <summary>
+        /// Downloads several files.
+        /// </summary>
+        /// <param name="objectIds">The object ids of items to download.</param>
+        /// <returns>The list of file paths.</returns>
+        Task<IEnumerable<string>> DownloadFiles(IEnumerable<string> objectIds);
+    }
+}
